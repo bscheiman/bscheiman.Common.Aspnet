c880c856c20e322cf29d65a103ceffb1cabcc62f
Boris Scheiman <bscheiman@gmail.com>
2015-01-02 19:23:38 -0600

    ModelBinder

---
+++ bscheiman.Common.Aspnet/Binder/JsonNetModelBinder.cs
@@ -0,0 +1,17 @@
+﻿#region
+using System.IO;
+using System.Web.Mvc;
+using bscheiman.Common.Extensions;
+
+#endregion
+
+namespace bscheiman.Common.Aspnet.Binder {
+    public class JsonNetModelBinder<T> : IModelBinder {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext) {
+            var request = controllerContext.HttpContext.Request;
+            request.InputStream.Seek(0, SeekOrigin.Begin);
+
+            return new StreamReader(request.InputStream).ReadToEnd().FromJson<T>();
+        }
+    }
+}
