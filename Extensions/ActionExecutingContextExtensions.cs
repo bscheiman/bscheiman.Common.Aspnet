7d33a29d1e39f593dea83aca9b0114c800bb4502
Boris Scheiman <bscheiman@gmail.com>
2015-01-03 15:05:51 -0600

    Logging extensions

---
+++ Extensions/ActionExecutingContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿#region
+using System.Diagnostics;
+using System.Web.Mvc;
+
+#endregion
+
+namespace bscheiman.Common.Aspnet.Extensions {
+    public static class ActionExecutingContextExtensions {
+        [DebuggerStepThrough]
+        public static void Log(this ActionExecutingContext ctx) {
+            if (ctx.HttpContext.Request.IsLocal)
+                return;
+
+            Util.Log.Info("Website hit from {0}: {1}/{2} ({3} - {4})", ctx.HttpContext.Request.UserHostAddress,
+                ctx.RouteData.Values["controller"], ctx.RouteData.Values["action"], ctx.HttpContext.Request.HttpMethod,
+                ctx.HttpContext.Request.UserAgent);
+        }
+    }
+}
---
+++ Extensions/HttpControllerContextExtensions.cs
@@ -0,0 +1,14 @@
+﻿#region
+using System.Net.Http;
+using System.Web.Http.Controllers;
+
+#endregion
+
+namespace bscheiman.Common.Aspnet.Extensions {
+    public static class HttpControllerContextExtensions {
+        public static void Log(this HttpControllerContext ctx) {
+            if (ctx.Request.IsLocal())
+                return;
+        }
+    }
+}
