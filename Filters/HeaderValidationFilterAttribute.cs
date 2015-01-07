1c5024333ce09280d9e114c8088bb5c33f31ca1e
Boris Scheiman <bscheiman@gmail.com>
2015-01-07 16:08:57 -0600

    HeaderValidationAttr

--- Filters/HeaderValidationFilterAttribute.cs
+++ Filters/HeaderValidationFilterAttribute.cs
@@ -1,52 +1,57 @@
 ﻿#region
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using bscheiman.Common.Aspnet.Extensions;
+using bscheiman.Common.Aspnet.Interfaces;
+using bscheiman.Common.Aspnet.Objects;
+using bscheiman.Common.Extensions;
+using bscheiman.Common.Util;
 
 #endregion
 
 namespace bscheiman.Common.Aspnet.Filters {
-    //public class HeaderValidationFilterAttribute<T> : ActionFilterAttribute where T : DbContext, new() {
-    //    public override void OnActionExecuting(HttpActionContext context) {
-    //        using (var database = new T()) {
-    //            string apiKey = context.Request.Headers.Get("X-ApiKey");
-    //            string hash = context.Request.Headers.Get("X-Hash");
-    //            long ts = context.Request.Headers.Get("X-Timestamp").ToInt64();
+    public abstract class HeaderValidationFilterAttribute : ActionFilterAttribute {
+        public override void OnActionExecuting(HttpActionContext context) {
+            string apiKey = context.Request.Headers.Get("X-ApiKey");
+            string hash = context.Request.Headers.Get("X-Hash");
+            long ts = context.Request.Headers.Get("X-Timestamp").ToInt64();
 
-    //            if (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(hash) || ts == 0) {
-    //                context.Response = CreateJson(403, "please specify ApiKey/Hash/Timestamp");
+            if (apiKey.IsNullOrEmpty() || hash.IsNullOrEmpty() || ts == 0) {
+                context.Response = CreateJson(403, "please specify ApiKey/Hash/Timestamp");
 
-    //                return;
-    //            }
+                return;
+            }
 
-    //            if ((DateUtil.Now - ts) > 300) {
-    //                context.Response = CreateJson(403, "too late, bro");
+            if ((DateUtil.Now - ts) > 300) {
+                context.Response = CreateJson(403, "too late, bro");
 
-    //                return;
-    //            }
+                return;
+            }
 
-    //            var business = database.Businesses.FirstOrDefault(b => b.ApiKey == apiKey);
+            var apiUser = GetApiUser(apiKey);
 
-    //            if (business == null) {
-    //                context.Response = CreateJson(403, "invalid api user");
+            if (apiUser == null)
+                context.Response = CreateJson(403, "invalid api user");
+            else {
+                string str = string.Format("{0}:{1}:{2}", apiKey, context.Request.RequestUri, ts);
+                string computedHash = str.ToHMAC256(apiUser.ApiSecret);
 
-    //                return;
-    //            }
+                if (computedHash != hash.ToUpper())
+                    context.Response = CreateJson(403, "invalid hash");
+            }
+        }
 
-    //            string str = string.Format("{0}:{1}:{2}", apiKey, context.Request.RequestUri, ts);
-    //            string computedHash = str.ToHMAC256(business.ApiSecret);
+        protected abstract IHasApiKeys GetApiUser(string apiKey);
 
-    //            if (computedHash != hash.ToUpper())
-    //                context.Response = CreateJson(403, "invalid hash");
-
-    //            // CacheUtil.Set(business);
-    //        }
-    //    }
-
-    //    private HttpResponseMessage CreateJson(int status, string error) {
-    //        return new HttpResponseMessage {
-    //            Content = new StringContent(new {
-    //                status,
-    //                error
-    //            }.ToJson())
-    //        };
-    //    }
-    //}
+        private HttpResponseMessage CreateJson(int status, string error) {
+            return new HttpResponseMessage {
+                Content = new StringContent(new BaseJson {
+                    Code = status,
+                    Valid = false,
+                    Message = error
+                }.ToJson())
+            };
+        }
+    }
 }
