75c0e3de7a298743a10da23a74d3adef8c21043e
Boris Scheiman <bscheiman@gmail.com>
2015-01-17 14:04:26 -0600

    AbstractHeaderValidationFilter

--- Filters/AbstractHeaderValidationFilterAttribute.cs
+++ Filters/AbstractHeaderValidationFilterAttribute.cs
@@ -11,13 +11,13 @@ using bscheiman.Common.Util;
 #endregion
 
 namespace bscheiman.Common.Aspnet.Filters {
-    public abstract class HeaderValidationFilterAttribute : ActionFilterAttribute {
+    public abstract class AbstractHeaderValidationFilterAttribute : ActionFilterAttribute {
         public override void OnActionExecuting(HttpActionContext context) {
             string apiKey = context.Request.Headers.Get("X-ApiKey");
             string hash = context.Request.Headers.Get("X-Hash");
             long ts = context.Request.Headers.Get("X-Timestamp").ToInt64();
 
-            if (apiKey.IsNullOrEmpty() || hash.IsNullOrEmpty() || ts == 0) {
+            if (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(hash) || ts == 0) {
                 context.Response = CreateJson(403, "please specify ApiKey/Hash/Timestamp");
 
                 return;
@@ -31,15 +31,17 @@ namespace bscheiman.Common.Aspnet.Filters {
 
             var apiUser = GetApiUser(apiKey);
 
-            if (apiUser == null)
+            if (apiUser == null) {
                 context.Response = CreateJson(403, "invalid api user");
-            else {
-                string str = string.Format("{0}:{1}:{2}", apiKey, context.Request.RequestUri, ts);
-                string computedHash = str.ToHMAC256(apiUser.ApiSecret);
 
-                if (computedHash != hash.ToUpper())
-                    context.Response = CreateJson(403, "invalid hash");
+                return;
             }
+
+            string str = string.Format("{0}:{1}:{2}", apiKey, context.Request.RequestUri, ts);
+            string computedHash = str.ToHMAC256(apiUser.ApiSecret);
+
+            if (computedHash != hash.ToUpper())
+                context.Response = CreateJson(403, "invalid hash");
         }
 
         protected abstract IHasApiKeys GetApiUser(string apiKey);
