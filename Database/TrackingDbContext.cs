a45d713572f0b94435209a8fba4179ae1adf0abd
Boris Scheiman <bscheiman@gmail.com>
2015-01-25 02:17:24 -0600

    TrackingDbContext<TUser>

--- Database/TrackingDbContext.cs
+++ Database/TrackingDbContext.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 #endregion
 
 namespace bscheiman.Common.Aspnet.Database {
+    // TODO: Refactor
     public class TrackingDbContext<TUser, TRole, TKey, TUserLogin, TUserRole, TUserClaim> :
         IdentityDbContext<TUser, TRole, TKey, TUserLogin, TUserRole, TUserClaim>
         where TUser : IdentityUser<TKey, TUserLogin, TUserRole, TUserClaim>
@@ -17,6 +18,56 @@ namespace bscheiman.Common.Aspnet.Database {
         where TUserLogin : IdentityUserLogin<TKey>
         where TUserRole : IdentityUserRole<TKey>
         where TUserClaim : IdentityUserClaim<TKey> {
+        public TrackingDbContext(string ctx) : base(ctx) {
+        }
+
+        public override int SaveChanges() {
+            var hasUsers =
+                ChangeTracker.Entries()
+                             .Where(x => x.Entity.Is<IHasUsers>() && (x.State == EntityState.Added || x.State == EntityState.Modified));
+            var hasDates =
+                ChangeTracker.Entries()
+                             .Where(x => x.Entity.Is<IHasDates>() && (x.State == EntityState.Added || x.State == EntityState.Modified));
+            var deleted = ChangeTracker.Entries().Where(x => x.Entity is ISoftDelete && x.State == EntityState.Deleted);
+
+            string currentUsername = HttpContext.Current != null && HttpContext.Current.User != null
+                ? HttpContext.Current.User.Identity.Name
+                : "Anonymous";
+
+            if (string.IsNullOrEmpty(currentUsername))
+                currentUsername = "API";
+
+            foreach (var entity in hasUsers) {
+                if (entity.State == EntityState.Added)
+                    ((IHasUsers) entity.Entity).UserCreated = currentUsername;
+
+                ((IHasUsers) entity.Entity).UserModified = currentUsername;
+            }
+
+            foreach (var entity in hasDates) {
+                if (entity.State == EntityState.Added)
+                    ((IHasDates) entity.Entity).DateCreated = DateUtil.NowDt;
+
+                ((IHasDates) entity.Entity).DateModified = DateUtil.NowDt;
+            }
+
+            foreach (var entity in deleted) {
+                ((ISoftDelete) entity.Entity).IsDeleted = true;
+
+                entity.State = EntityState.Modified;
+            }
+
+            return base.SaveChanges();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Add(new ForeignKeyNamingConvention());
+        }
+    }
+
+    public class TrackingDbContext<TUser> : IdentityDbContext<TUser> where TUser : IdentityUser {
         public TrackingDbContext(string ctx) : base(ctx) {
         }
 
