29a068fbf8366bc9fb1efde978fa411bb33b1676
Boris Scheiman <bscheiman@gmail.com>
2015-03-06 13:26:51 -0600

    Hidden attributes

--- Database/BaseEntity.cs
+++ Database/BaseEntity.cs
@@ -1,5 +1,7 @@
 ﻿#region
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 using bscheiman.Common.Aspnet.Interfaces;
 
 #endregion
@@ -9,11 +11,22 @@ namespace bscheiman.Common.Aspnet.Database {
     }
 
     public class BaseEntity<T> : IHasUsers, IHasDates, ISoftDelete {
+        [Editable(false), HiddenInput(DisplayValue = false)]        
         public T Id { get; set; }
+
+        [Editable(false), HiddenInput(DisplayValue = false)]        
         public DateTime DateCreated { get; set; }
+
+        [Editable(false), HiddenInput(DisplayValue = false)]
         public DateTime DateModified { get; set; }
+
+        [Editable(false), HiddenInput(DisplayValue = false)]
         public string UserCreated { get; set; }
+
+        [Editable(false), HiddenInput(DisplayValue = false)]
         public string UserModified { get; set; }
+
+        [Editable(false), HiddenInput(DisplayValue = false)]
         public bool IsDeleted { get; set; }
     }
 }
